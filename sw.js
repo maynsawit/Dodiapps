087f84359b1ebd6b72b82301a9f6eab2d340cb22
maynsawit <maynsawit@gmail.com>
2026-03-13 20:56:26 +0700

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,55 @@
+const CACHE_NAME = "dodiapps-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+
+// INSTALL
+self.addEventListener("install", event => {
+
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+
+});
+
+
+// ACTIVATE
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+
+  return self.clients.claim();
+
+});
+
+
+// FETCH
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+
+});
